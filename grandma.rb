070107f058472ad39803a3baa24a2995985fe269
Cailie Skelton <cskelton707@gmail.com>
2020-04-19 01:19:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase !=  "HUH?! SPEAK UP, SONNY!"
+  elsif phrase != phrase.upcase
+  "HUH?! SPEAK UP, SONNY!"
   end
 end
